1dc7ead202252934e47c80b76ff83b33f8f9969d
Travis A Mullen <travis.a.mullen@gmail.com>
2025-05-06 09:12:39 -0400

    Created public list and detail views and templates for students to view courses.

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from courses.views import CourseListView
 
 urlpatterns = [
     path('mine/',
@@ -56,4 +57,15 @@ urlpatterns = [
         views.ContentOrderView.as_view(),
         name='content_order'
     ),
+    path(
+        'subject/<slug:subject>/',
+        views.CourseListView.as_view(),
+        name='course_list_subject'
+    ),
+    path(
+        '<slug:slug>/',
+        views.CourseDetailView.as_view(),
+        name='course_detail'
+    ),
+    path('', CourseListView.as_view(), name='course_list'),
 ]
--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,17 +1,19 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin
 )
 from django.apps import apps
 from django.forms.models import modelform_factory
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.base import TemplateResponseMixin, View
 from .forms import ModuleFormSet
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from django.db.models import Count
 
 # Create your views here.
 
@@ -176,3 +178,29 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
                 module__course__owner=request.user
             ).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+            total_courses = Count('courses')
+        )
+        courses = Course.objects.annotate(
+            total_modules = Count('modules')
+        )
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response(
+            {
+                'subjects': subjects,
+                'subject': subject,
+                'courses': courses
+            }
+        )
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
