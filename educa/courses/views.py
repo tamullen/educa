f39987b66db816bdc3fbc41226a728d23a74333e
Travis A Mullen <travis.a.mullen@gmail.com>
2025-05-06 09:53:41 -0400

    Added student registration and enrollment.

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -14,6 +14,7 @@ from django.views.generic.base import TemplateResponseMixin, View
 from .forms import ModuleFormSet
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from django.db.models import Count
+from students.forms import CourseEnrollForm
 
 # Create your views here.
 
@@ -204,3 +205,10 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(
+            initial={'course': self.object}
+        )
+        return context
